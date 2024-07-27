ba145cd1dca59e1f794e71080e773921e1793ede
Munees <muneesmmm@gmail.com>
2024-07-27 22:39:13 +0530

    updated

--- netlify/express-handler.js
+++ netlify/express-handler.js
@@ -1,58 +1,41 @@
-// server.js
+// netlify/express-handler.js
+const serverless = require("serverless-http");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const authHelpers = require("./helpers/auth");
-const userController = require("./controllers/userController");
-const transactionController = require("./controllers/transactionController");
-const customerController = require("./controllers/customerController");
-const orderController = require("./controllers/orderController");
-const dashboardController = require("./controllers/dashboardController"); 
-const uploadHelper = require("./helpers/s3Upload");
+const authHelpers = require("../helpers/auth");
+const userController = require("../controllers/userController");
+const transactionController = require("../controllers/transactionController");
+const customerController = require("../controllers/customerController");
+const orderController = require("../controllers/orderController");
+const dashboardController = require("../controllers/dashboardController");
 const cors = require("cors");
-const emailController = require("./controllers/emailController");
-const smsController = require("./controllers/smsController");
+const emailController = require("../controllers/emailController");
+const smsController = require("../controllers/smsController");
 
-require("dotenv").config();
 const app = express();
-const port = 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:8080", // Replace with your frontend's origin URL
-        allowedHeaders: ["Content-Type", "Authorization"], // Add any additional headers you want to allow
+    origin: "http://localhost:8080",
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.use((req, res, next) => {
-res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-res.setHeader('Access-Control-Allow-Credentials', true);
-next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB using Mongoose
 mongoose
-  .connect(
-    "mongodb+srv://munees:FKqh2rPzFRBlRzQR@hexpeak.cpln38x.mongodb.net/?retryWrites=true&w=majority&appName=hexpeak",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 // Routes
-// app.use(authHelpers.authenticateToken);
-app.post(
-  "/register",
-  userController.registerUser
-);
-
+app.post("/register", userController.registerUser);
 app.post("/login", userController.loginUser);
 app.post("/admin-login", userController.adminLogin);
 app.post("/get-users", userController.getUsers);
@@ -61,7 +44,6 @@ app.post("/delete-user", userController.deleteUserbyId);
 app.post("/update/:id", userController.updateUser);
 app.post("/update-payment/:id", userController.updatePayment);
 app.get("/protected", authHelpers.authenticateToken, (req, res) => {
-  // Access the authenticated user using req.user
   res.json({
     message: "Protected route accessed successfully",
     user: req.user,
@@ -74,17 +56,13 @@ app.post("/get-transactions/:id", transactionController.getTransactionById);
 app.post("/create-payment", transactionController.createPayment);
 app.post("/create-paymentintent", transactionController.createPaymentIntent);
 app.post("/payment-confirm", transactionController.addTransaction);
-app.post(
-  "/delete-transactions/:id",
-  transactionController.deleteTransactionById
-);
+app.post("/delete-transactions/:id", transactionController.deleteTransactionById);
 app.post('/createCustomer', customerController.addCustomer);
-app.post('/customer/:userId',customerController.getCustomerById)
+app.post('/customer/:userId', customerController.getCustomerById);
 app.post("/dashboard-data", dashboardController.getDashBoardData);
-app.post('/send-email', emailController.sendEmail); 
+app.post('/send-email', emailController.sendEmail);
 app.post('/sendSMS', (req, res) => {
   const { to, message } = req.body;
-
   smsController.sendSMS(to, message)
     .then(message => {
       console.log('Message sent! SID:', message.sid);
@@ -94,12 +72,8 @@ app.post('/sendSMS', (req, res) => {
       console.error('Error sending message:', error);
       res.status(500).send('Failed to send message');
     });
-  });
-
-
-  app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 app.post("/get-orders", orderController.getOrders);
 app.post("/get-staff-orders", orderController.getOrderByStaffId);
-// app.post("/total-amount", orderController.getTotalAmountByPaymentType);
+
+module.exports.handler = serverless(app);
